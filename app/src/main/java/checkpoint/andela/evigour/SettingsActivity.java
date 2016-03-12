ea0b3c577078280cd22512373bc80a1763520b7c
andela-shassan <hassan.ayomon@andela.com>
2016-03-12 02:15:07 +0100

    [Finished #115275093] Setup navigation arrow for setting activity and optimize the code

--- app/src/main/java/checkpoint/andela/evigour/SettingsActivity.java
+++ app/src/main/java/checkpoint/andela/evigour/SettingsActivity.java
@@ -248,4 +248,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
