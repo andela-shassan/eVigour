d3171d848d020a14e5dcc3626ee39143f5882eff
andela-shassan <hassan.ayomon@andela.com>
2016-03-16 17:48:12 +0100

    [start #115649287] Create the Graph view activity

---
+++ app/src/main/java/checkpoint/andela/graph/ReportGraph.java
@@ -0,0 +1,13 @@
+package checkpoint.andela.graph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportGraph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_graph);
+    }
+}
