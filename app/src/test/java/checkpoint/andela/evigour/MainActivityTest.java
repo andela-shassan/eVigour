80e456519103237cd1f253400e94e4638e508d12
andela-shassan <hassan.ayomon@andela.com>
2016-03-21 15:00:28 +0100

    [Finishes \#115275193] Update the MainActivityTest class

--- app/src/test/java/checkpoint/andela/evigour/MainActivityTest.java
+++ app/src/test/java/checkpoint/andela/evigour/MainActivityTest.java
@@ -1,28 +1,47 @@
 package checkpoint.andela.evigour;
 
+import android.os.Build;
+import android.view.MenuItem;
+import android.view.View;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.fakes.RoboMenuItem;
 
-import static org.junit.Assert.*;
+import java.util.Calendar;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
- * Created by andela on 20/03/2016.
+ * Created by andela on 12/03/2016.
  */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
 public class MainActivityTest {
+    private MainActivity activity;
 
     @Before
     public void setUp() throws Exception {
-
+        activity = Robolectric.setupActivity(MainActivity.class);
     }
 
     @Test
     public void testOnCreate() throws Exception {
-
+        View view = activity.findViewById(R.id.drawer_layout);
+        assertNotNull(view);
     }
 
     @Test
     public void testOnCreateOptionsMenu() throws Exception {
-
+        MenuItem setting = new RoboMenuItem(R.menu.main);
+        assertNotNull(setting);
     }
 
     @Test
@@ -32,6 +51,8 @@ public class MainActivityTest {
 
     @Test
     public void testOnNavigationItemSelected() throws Exception {
+        MenuItem drawer = new RoboMenuItem(R.menu.activity_main_drawer);
+        assertNotNull(drawer);
 
     }
 
@@ -61,8 +82,15 @@ public class MainActivityTest {
     }
 
     @Test
-    public void testSetReminder() throws Exception {
+    public void testSetCalendar() throws Exception {
+        Calendar calendar = activity.setCalendar(10, 30);
+        assertTrue(calendar.isSet(Calendar.HOUR));
+        assertTrue(calendar.isSet(Calendar.MINUTE));
+    }
 
+    @Test
+    public void testSetAlarm() throws Exception {
+      //  AlarmManager alarmManager = activity.setAlarm();
     }
 
     @Test
